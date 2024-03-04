4fd017d0c631568e054bea970bcde1d0bf912c07
Andrew A <72630976+program-terminated@users.noreply.github.com>
2024-03-03 19:26:34 -0500

    Update parent_selection.py

--- model/parent_selection.py
+++ model/parent_selection.py
@@ -1,25 +1,15 @@
-import random
+import numpy as np
 
-def parent_select(population, subset_size):
-    subset_point = random.randrange(len(population))
-    subset = []
-    # If the subset_point selected is greater than the length of the population with itself subtracted,
-    # then the ending index of the subset would be out of range. Therefore, we will need to wrap around
-    # the population.
-    if subset_point >= len(population)-subset_size:
-        index_to_add = subset_point
-        while len(subset) < subset_size:
-            try:
-                # Add next element to the subset
-                subset.append(population[index_to_add])
-                index_to_add += 1
-            except:
-                # Wrap around
-                index_to_add -= len(population)
-
-    # No need to wrap around
-    else:
-        subset = population[subset_point:subset_point+subset_size]
+# Parent selection - tournament
+def parent_select(population, tournament_size):
+    
+    # Get a random subset of the population to compete in the tournament.
+    pop_subset = np.random.choice(population, tournament_size, False)
 
     # Subset has been selected, pick the 2 fittest.
     # Fitness function is TBD, so this cannot be implemented just yet.
+    # The returned value of this function call will be the fitness of all the individuals.
+    # This function will return the two fittest individuals.
+    
+    # indivs_fitness = [], contains all the fitnesses. Indices of the two highest fitnesses are the indices of the
+        # individuals we will select as parents.
