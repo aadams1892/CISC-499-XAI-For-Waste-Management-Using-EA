acd423cb7fadc74bb735392a7dc09f8d77622e58
Andrew A <72630976+program-terminated@users.noreply.github.com>
2024-03-03 19:33:14 -0500

    Update crossover.py

--- model/crossover.py
+++ model/crossover.py
@@ -9,17 +9,19 @@ def crossover(parent1, parent2):
     if crossover_rate > crossover_occurs:
 
         # Perform PMX on offspring1.
-        offspring1 = pmx(parent1, parent2)
+        offspring1_raw = pmx(parent1, parent2)
 
         # Perform PMX on offspring2, with the role of the parents reversed from offspring1.
-        offspring2 = pmx(parent2, parent1)
+        offspring2_raw = pmx(parent2, parent1)
 
-        offspring_base = [offspring1, offspring2]
     # Crossover does not occur, directly copy parents as offspring.
     else:
-        offspring_base = [parent1, parent2]
+        offspring1_raw = parent1
+        offspring2_raw = parent2
 
     # Call mutation function on both offspring.
-    offspring = [mutate(offspring_base(0)), mutate(offspring_base(1))]
+    offspring1 = mutate(offspring1_raw)
+    offspring2 = mutate(offspring2_raw)
+    offspring = [offspring1, offspring2]
 
     return offspring
