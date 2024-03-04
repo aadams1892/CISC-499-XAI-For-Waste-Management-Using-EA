ef08c94fa0a9721822481e29539e4b7b7ce7b36c
Andrew A <72630976+program-terminated@users.noreply.github.com>
2024-03-03 19:22:41 -0500

    Update crossover.py

--- model/crossover.py
+++ model/crossover.py
@@ -1,9 +1,25 @@
-# This function will perform crossover on two parents according to certain crossover strategy
+# This function will go through the crossover process, from calculating if it will occur to mutating the offspring.
 def crossover(parent1, parent2):
-    offspring = [] # Where the two offspring will go
-    crossover_rate = 0.9 # The probability that crossover will occur. Otherwise, the parents will be directly copied.
-    # Perform crossover
-    # Call mutation function on both offspring
-    # Preserve adjacencies, discrete
+    offspring_base = [] # Where the two offspring will go
+    crossover_rate = 0.9 # The probability that crossover will occur.
+    crossover_occurs = random.random()
+
+    print(crossover_rate, crossover_occurs)
+    # Perform partially mapped crossover (PMX). PMX chosen for adjacency preservation.
+    if crossover_rate > crossover_occurs:
+
+        # Perform PMX on offspring1.
+        offspring1 = pmx(parent1, parent2)
+
+        # Perform PMX on offspring2, with the role of the parents reversed from offspring1.
+        offspring2 = pmx(parent2, parent1)
+
+        offspring_base = [offspring1, offspring2]
+    # Crossover does not occur, directly copy parents as offspring.
+    else:
+        offspring_base = [parent1, parent2]
+
+    # Call mutation function on both offspring.
+    offspring = [mutate(offspring_base(0)), mutate(offspring_base(1))]
 
     return offspring
