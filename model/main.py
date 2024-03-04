ff134d3e785c845afd6039b4b8b997f7fb93b140
Andrew A <72630976+program-terminated@users.noreply.github.com>
2024-03-03 19:28:06 -0500

    Update main.py

--- model/main.py
+++ model/main.py
@@ -9,14 +9,21 @@ import merge_sort as ms
 
 # Main
 def main():
-    hardcode = 0
+    mutate_test = 0
+    sort_test = 0
+    crossover_test = 1
 
-    pop1 = [45, 43, 2, 77, 1, 75, 77, 42, 39]
-    sorted_pop = ms.sort(pop1)
-    print(sorted_pop)
+    if sort_test:
+        pop1 = [45, 43, 2, 77, 1, 75, 77, 42, 39]
+        sorted_pop = ms(pop1)
+        print(sorted_pop)
 
-    tester = [1,2,3,4,5,6,7,8,9]
-    for _ in range(5):
-        mut = mt.mutate(tester)
-        print(tester, "-->", mut)
+    if mutate_test:
+        tester = [1,2,3,4,5,6,7,8,9]
+        for _ in range(5):
+            mutation = mt(tester)
+            print(tester, "-->", mutation)
+
+    if crossover_test:
+        co([1,2,3], [5,7,9])
 main()
