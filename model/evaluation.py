77437ff10a0035191cf8d1da8225d46c9e7513a6
Lucas <76797507+lucster10@users.noreply.github.com>
2024-04-05 23:06:36 -0400

    XAI methods re-ran, tidied up Jupyter notebook output

--- model/evaluation.py
+++ model/evaluation.py
@@ -1,4 +1,4 @@
-def fitness(individual, bin_info):
+def fitness(individual, bin_info, eval_type):
 
     # Computes fitness using individual length, and total distance 
 
@@ -6,16 +6,16 @@ def fitness(individual, bin_info):
     for bin in range(len(individual)-1):
         # Depending on how much the truck must turn, distance in scaled accordingly.
         turn = 1
-        if (abs(bin_info.bins[bin][0] - bin_info.bins[bin+1][0]) == 1):
-            turn = 1.25
-        elif (abs(bin_info.bins[bin][0] - bin_info.bins[bin+1][0]) == 2):
-            turn = 1.5
-        elif (abs(bin_info.bins[bin][0] - bin_info.bins[bin+1][0]) == 3): 
-            turn = 1.75
-        elif (abs(bin_info.bins[bin][0] - bin_info.bins[bin+1][0]) == 4):
-            turn = 2
+        if(eval_type == 1):
+            if (abs(bin_info.bins[bin][0] - bin_info.bins[bin+1][0]) == 1):
+                turn = 1.25
+            elif (abs(bin_info.bins[bin][0] - bin_info.bins[bin+1][0]) == 2):
+                turn = 1.5
+            elif (abs(bin_info.bins[bin][0] - bin_info.bins[bin+1][0]) == 3): 
+                turn = 1.75
+            elif (abs(bin_info.bins[bin][0] - bin_info.bins[bin+1][0]) == 4):
+                turn = 2
         dist += bin_info.get_dist(individual[bin], individual[bin+1]) * turn 
-
 
     if (dist > 0):
         fit = len(individual)/dist
